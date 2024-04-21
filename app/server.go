578aecb4e98d1c2904e7a105c36b193a91194e03
responsetime <aayush.bhattacharjee2002@gmail.com>
2024-04-21 19:27:49 +0530

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
 )
+
+const (
+	CLRF      = "\r\n"
+	OK        = "HTTP/1.1 200 OK" + CLRF + CLRF
+	NOT_FOUND = "HTTP/1.1 404 Not Found" + CLRF + CLRF
+)
+
+type ReqStatusLine struct {
+	Method       string
+	Path         string
+	HTTP_version string
+}
+
+func extract_statusline(Method, Path, Version string) *ReqStatusLine {
+	return &ReqStatusLine{Method: Method, Path: Path, HTTP_version: Version}
+}
+
+func handle(con net.Conn) {
+	buffer := make([]byte, 1024)
+	con.Read(buffer)
+	req := bytes.Split(buffer, []byte(CLRF))
+	statusL := bytes.Split(req[0], []byte(" "))
+	line := extract_statusline(string(statusL[0]), string(statusL[1]), string(statusL[2]))
+
+	if line.Path == "/" {
+		con.Write([]byte(OK))
+	} else {
+		con.Write([]byte(NOT_FOUND))
+	}
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -25,4 +56,5 @@ func main() {
 	}
 
 	r.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	handle(r)
 }
