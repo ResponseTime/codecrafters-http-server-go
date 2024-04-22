753388ffe6bb142ef7484728d514c31fb45420c4
responsetime <aayush.bhattacharjee2002@gmail.com>
2024-04-22 20:44:38 +0530

    pass 4th stage

--- app/server.go
+++ app/server.go
@@ -74,7 +74,9 @@ func handle(con net.Conn) {
 		strings.Split(line.Path, "/")[1],
 	)
 	resStatusLine := ResponseStatusLine{Version: "HTTP/1.1", Status: "200", Ok: "OK"}
-	if parsedPath[0] != "" && parsedPath[0] != "echo" {
+	if parsedPath[0] != "" && parsedPath[0] == "echo" {
+		resStatusLine.Status = "200"
+	} else {
 		resStatusLine.Status = "404"
 	}
 
